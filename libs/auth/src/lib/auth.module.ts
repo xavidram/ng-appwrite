51a90aa9bf02e75be5b1551b7819b2f3c966cfbe
Xavidram <xavidram@hotmail.com>
2020-09-27 05:14:23 -0500

    added module declaration

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,14 +1,36 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {} from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { LoginComponent } from './containers/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxsModule } from '@ngxs/store';
+import { AuthState } from './state/auth.state';
+import { AuthHandler } from './handlers/auth.handler';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild([])
+    ReactiveFormsModule,
+    FormsModule,
+    NgxsModule.forFeature([AuthState]),
+    RouterModule.forChild([
+      {
+        path: 'login',
+        component: LoginComponent,
+        data: { title: 'Login' }
+      }
+    ])
   ],
   declarations: [LoginFormComponent, LoginComponent],
 })
-export class AuthModule {}
+export class AuthModule {
+  static forRoot(): ModuleWithProviders<AuthModule> {
+    return {
+      ngModule: AuthModule,
+      providers: []
+    }
+  }
+
+  constructor(authHandler: AuthHandler) {}
+}
--- libs/auth/src/lib/guards/auth.guard.ts
+++ libs/auth/src/lib/guards/auth.guard.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
+import { LoginRedirect } from '../state/auth.actions';
+import { AuthState } from '../state/auth.state';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  
+  constructor(private store: Store) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    
+      const loggedIn: boolean = this.store.selectSnapshot(AuthState.isLoggedIn);
+
+      if(loggedIn) {
+        return true;
+      } else {
+        this.store.dispatch(new LoginRedirect());
+        return false;
+      }
   }
   
 }
