d7a78eab749cf7df9b76a4ded10681ad09becabd
Xavidram <xavidram@hotmail.com>
2020-09-27 23:15:05 -0500

    starting setup for account service

---
+++ libs/angular-appwrite/src/lib/services/account.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { IAppwriteAccount } from '../models/IAppwriteAccount.model';
+
+@Injectable()
+export class AppwriteAccount implements IAppwriteAccount {
+    constructor() {}
+
+    async create(email: string, password: string, name: string): Promise<any> {}
+    async createSession(email: string, password: string): Promise<any> {}
+    async createOAuth2Session(provider: string, success?: string, failure?: string): Promise<any> {}
+    async get(): Promise<any> {}
+    async getPrefs(): Promise<any> {}
+    async getSessions(): Promise<any> {}
+    async getLogs(): Promise<any> {}
+    async updateName(name: string): Promise<any> {}
+    async updatePassword(password: string, oldPassword: string): Promise<any> {}
+    async updateEmail(email: string, password: string): Promise<any> {}
+    async updatePrefs(prefs: any): Promise<any> {}
+    async delete(): Promise<any> {}
+    async deleteSession(sessionId: string): Promise<any> {}
+    async deleteSessions(): Promise<any> {}
+    async createRecovery(email: string, url: string): Promise<any> {}
+    async updateRecovery(userId: string, secret: string, password: string, passwordAgain: string): Promise<any> {}
+    async createVerification(url: string): Promise<any> {}
+    async updateVerification(userId: string, secret: string): Promise<any> {}
+}
