089f08bdd1f51417ad13a4090a2c474b7bbc8352
Xavidram <xavidram@hotmail.com>
2020-09-28 22:38:08 -0500

    adding appwrite service proviced by eldad1 on github

--- libs/angular-appwrite/src/index.ts
+++ libs/angular-appwrite/src/index.ts
@@ -1,7 +1,24 @@
-import { AppwriteAccount as account } from './lib/services/account.service';
+/** 
+ * This module code was extracted from the typescript 
+ * generator generated sdk-for-typescript by @eldadfux 
+ * 
+ * repo: https://github.com/aw-tests/sdk-for-typescript
+ * */
 
-const appwrite = {
-    account
-}
+import { Client } from "./lib/client";
+import { Account } from "./lib/services/account.service";
+import { Avatars } from "./lib/services/avatars.service";
+import { Database } from "./lib/services/database.service";
+import { Locale } from "./lib/services/locale.service";
+import { Storage } from "./lib/services/storage.service";
+import { Teams } from "./lib/services/teams.service";
 
-export default appwrite;
+export {
+    Client,
+    Account,
+    Avatars,
+    Database,
+    Locale,
+    Storage,
+    Teams,
+};
--- libs/angular-appwrite/src/lib/models/IAppwrite.model.ts
+++ libs/angular-appwrite/src/lib/models/IAppwrite.model.ts
@@ -1,7 +1,11 @@
-export interface IAppwrite {
+export interface IAppwriteClient {
     setEndpoint(endpoint: string): this;
     setProject(projectId: string): this;
     setLocale(locale: string): this;
     setKey(key: string): this;
     setMode(mode: string): this;
+}
+
+export class IAppwriteService {
+    client: IAppwriteClient;
 }
